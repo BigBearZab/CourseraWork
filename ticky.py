460f8138c033a6f9fe1a7b2bf0563cd04f4b014e
BigBearZab <61662036+BigBearZab@users.noreply.github.com>
2022-01-12 16:24:42 +0000

    Update ticky.py

--- ticky.py
+++ ticky.py
@@ -9,18 +9,37 @@ user_dict = {}
 #     if item in dict.keys():
 
 def findUser(line):
-    return re.findall('\(.+\)', line)[0].strip("()")
+    username = re.search('\((.+)\)', line)
+    if username:
+        return username.group(1)
 
 def findError(line):
-    return re.search("ERROR: ()[\w ']+) \(",line)[0].strip("ERROR: ").strip( " (")
+    error = re.search("ERROR: ([\w ]+) \(", line)
+    if error:
+        return error.group(1)
 
 for line in open('sys.log', 'r'):
     if 'INFO' in line:
-        print('INFO ' + line)
         user = findUser(line)
         print(user)
+        if user in user_dict.keys():
+            user_dict[user]['INFO'] += 1
+        else:
+             user_dict[user] = {'INFO':1,'ERROR':0}
+
     elif 'ERROR' in line:
-        print('ERROR ' + line)
         user = findUser(line)
         print(user)
+        if user in user_dict.keys():
+            user_dict[user]['ERROR'] += 1
+        else:
+             user_dict[user] = {'INFO':0,'ERROR':1}
         print(findError(line))
+        error = findError(line)
+        if error in error_dict.keys():
+            error_dict[error] += 1
+        else:
+            error_dict[error] = 1
+
+print(error_dict)
+print(user_dict)
